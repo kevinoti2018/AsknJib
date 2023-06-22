77e692edeaf5929a9955ad69e141b38b59b13990
kevin otieno <kelvinotix@gmail.com>
2023-06-22 14:52:54 +0300

    background service complete

--- BackgroundServices/src/Email/reset.ts
+++ BackgroundServices/src/Email/reset.ts
@@ -1,44 +1,46 @@
-import { sendMail } from "../Helpers/sendMail";
-import { sqlConfig } from "../config";
-import mssql from 'mssql'
-import ejs from 'ejs'
+import { sendMail } from '../Helpers/sendMail';
+import { sqlConfig } from '../config';
+import mssql from 'mssql';
+
 interface User {
-    User_Id: string;
-    Username: string;
-    Email: string;
-    Password: string;
-    IsAdmin: boolean;
-    isDeleted: boolean;
-    ResetSuccess: boolean;
-    isSent: boolean;
-  }
-const sendResetEmail = async()=>{
-    const pool = await mssql.connect(sqlConfig)
-    const users:User[]= await(await pool.request().
-    query("SELECT * FROM USERS WHERE ResetSuccess ='1'")).recordset
-    // console.log(users);
-
-for(let user of users){
-    ejs.renderFile('Templates/resetPassword.ejs',{name:user.Username}, async(error, html)=>{
-    const message = {
-    from: process.env.EMAIL,
-    to: user.Email,
-    subject: "Password Reset",
-    html
-};
-
-console.log(html);
-
- try {
-await sendMail(message) 
-await pool.request().query(`UPDATE USERS
-SET resetSuccess = 0 WHERE User_Id ='${user.User_Id}'`)
- } catch (error) {
-    console.log(error);
-    
- }  
-})
-}    
+  User_Id: string;
+  Username: string;
+  Email: string;
+  Password: string;
+  IsAdmin: boolean;
+  isDeleted: boolean;
+  ResetSuccess: boolean;
+  isSent: boolean;
 }
 
-export default sendResetEmail
+const sendResetEmail = async () => {
+  const pool = await mssql.connect(sqlConfig);
+  const users: User[] = await (
+    await pool.request().query("SELECT * FROM USERS WHERE ResetSuccess ='0'")
+  ).recordset;
+
+  const resetSuccessMessage = 'Your password has been successfully reset.';
+
+  for (let user of users) {
+    const message = {
+      from: process.env.EMAIL,
+      to: user.Email,
+      subject: 'Password Reset',
+      html: `<p>${resetSuccessMessage}</p>`,
+    };
+
+    try {
+      await sendMail(message);
+      await pool
+        .request()
+        .query(
+          `UPDATE USERS SET ResetSuccess =' 1 'WHERE User_Id ='${user.User_Id}'`
+        );
+      console.log(resetSuccessMessage);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+export default sendResetEmail;
