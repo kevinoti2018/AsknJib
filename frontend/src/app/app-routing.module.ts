050cde4b7b709287cfd90aa00db07f13f678ac2e
kevin otieno <kelvinotix@gmail.com>
2023-06-27 13:32:48 +0300

    deepscan updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { LandingComponent } from './components/homepage/landing/landing.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { ResetComponent } from './components/reset/reset.component';
 
 const routes: Routes = [
   {path:'', component:LandingComponent},
