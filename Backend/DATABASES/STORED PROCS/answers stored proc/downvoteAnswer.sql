fdfe4408fe377b997ac97aeeb3f664d8ad635311
kevin otieno <kelvinotix@gmail.com>
2023-06-27 12:46:43 +0300

    voting on answer

--- Backend/DATABASES/STORED PROCS/answers stored proc/downvoteAnswer.sql
+++ Backend/DATABASES/STORED PROCS/answers stored proc/downvoteAnswer.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE DownvoteAnswer1
+CREATE PROCEDURE DownvoteAnswers1
     @User_Id VARCHAR(100),
     @AnswerId VARCHAR(100)
 AS
@@ -20,8 +20,8 @@ BEGIN
         )
         BEGIN
             -- Insert the vote record if the user is different from the answer owner
-            INSERT INTO VOTES (User_Id, AnswerId)
-            VALUES (@User_Id, @AnswerId);
+            INSERT INTO VOTES (User_Id, AnswerId, VoteType)
+            VALUES (@User_Id, @AnswerId, 'downvote');
 
             -- Decrement the VoteCount in the ANSWERS table, retain as 0 if already 0 or negative
             UPDATE ANSWERS
@@ -32,7 +32,7 @@ BEGIN
             WHERE AnswerId = @AnswerId;
 
             -- Return success status
-            SELECT 'Vote recorded successfully.' AS Result;
+            SELECT 'Downvote recorded successfully.' AS Result;
         END
         ELSE
         BEGIN
@@ -42,7 +42,36 @@ BEGIN
     END
     ELSE
     BEGIN
-        -- Return error status if the user has already voted for the answer
-        SELECT 'User has already voted for this answer.' AS Result;
+        -- Check if the user's previous vote was an upvote
+        IF EXISTS (
+            SELECT 1
+            FROM VOTES
+            WHERE User_Id = @User_Id
+            AND AnswerId = @AnswerId
+            AND VoteType = 'upvote'
+        )
+        BEGIN
+            -- Update the existing upvote record to a downvote
+            UPDATE VOTES
+            SET VoteType = 'downvote'
+            WHERE User_Id = @User_Id
+            AND AnswerId = @AnswerId;
+
+            -- Decrement the VoteCount in the ANSWERS table
+            UPDATE ANSWERS
+            SET VoteCount = CASE
+                WHEN VoteCount <= 0 THEN 0
+                ELSE VoteCount - 1
+                END
+            WHERE AnswerId = @AnswerId;
+
+            -- Return success status
+            SELECT 'Downvote recorded successfully.' AS Result;
+        END
+        ELSE
+        BEGIN
+            -- Return error status if the user has already downvoted for the answer
+            SELECT 'User has already downvoted for this answer.' AS Result;
+        END
     END
 END;
